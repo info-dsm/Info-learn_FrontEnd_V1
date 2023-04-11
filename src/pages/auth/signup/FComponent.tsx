4626f50b413659586ad5fe8a8683594c724f7e81
eternrust <eternrust@dsm.hs.kr>
2023-04-11 22:54:34 +0900

    SignUp 퍼블리싱 완료

---
+++ src/pages/auth/signup/FComponent.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import InputBox from "../../../components/inputBox";
+import { PrimaryButton } from "../../../components/PrimaryButton";
+import BottomTitle from "./BottomTitle";
+import * as _ from './style'
+
+type ValueType = 'id' | 'password' | 'email' | 'authentication' | 'nickname';
+interface ValueProps {
+  name: ValueType;
+  data: string;
+}
+
+interface ComponentsProps {
+    value: { [key in ValueType]: string };
+    change: ({ name, data }: ValueProps) => void;
+    setIndex: React.Dispatch<React.SetStateAction<number>>;
+    Index: number;
+  }
+
+const FConponent = ({ value, change, Index, setIndex }: ComponentsProps) => {
+    const {id, password} = value;
+    return (
+      <_.SignBox bool={Index === 0}>
+        <_.SignUpMain>
+          <_.Title>회원가입</_.Title>
+          <_.SubTitle>회원가입을 하여 서비스를 이용해보세요</_.SubTitle>
+          <_.Flexbox>
+            <InputBox
+              Title='아이디'
+              name='id'
+              placeholder='아이디를 입력해주세요'
+              change={change}
+              value={id}
+            />
+            <InputBox
+              Title='비밀번호'
+              name='password'
+              placeholder='영•숫자•기호 포함 8자 이상'
+              eyes={false}
+              change={change}
+              value={password}
+            />
+          </_.Flexbox>
+          <PrimaryButton onClick={() => id && password  && setIndex(Index + 1)} width="400px" height='47px'>
+            다음
+          </PrimaryButton>
+          <BottomTitle LText='이미 회원이신가요?' RText='로그인' />
+        </_.SignUpMain>
+      </_.SignBox>
+    )
+  }
+
+  export default FConponent;
---
+++ src/pages/auth/signup/Signup.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import 'remixicon/fonts/remixicon.css';
+import FConponent from "./FComponent";
+import SConponent from "./SComponent";
+import LConponent from "./LComponent";
+import * as _ from './style'
+
+type ValueType = 'id' | 'password' | 'email' | 'authentication' | 'nickname';
+interface ValueProps {
+  name: ValueType;
+  data: string;
+}
+
+const Signup = () => {
+  const [Index, setIndex] = useState(0);
+  const [ImgFile, setImg] = useState<File>();
+  const [ImgString, setImgString] = useState<string | ArrayBuffer | null>(null)
+  const [value, setValue] = useState<{
+    [key in ValueType]: string
+  }>({
+    id: '',
+    password: '',
+    email: '',
+    authentication: '',
+    nickname: ''
+  });
+
+  const ChangeValue = ({ name, data }: ValueProps): void => {
+    setValue((value)=>{return{
+      ...value,
+      [name]: data
+    }})
+  }
+
+  const ComponentValue = {
+    value,
+    change: ChangeValue,
+    Index,
+    setIndex
+  }
+
+  const LComponentValue = {
+    ...ComponentValue,
+    setImg,
+    ImgString,
+    setImgString
+  }
+
+  return (
+    <>
+      <_.TopCircle />
+      <_.BottomCircle />
+      <_.BlurBox>
+        <FConponent {...ComponentValue} />
+        <SConponent {...ComponentValue} />
+        <LConponent {...LComponentValue} />
+      </_.BlurBox>
+    </>
+  )
+}
+
+export default Signup;
